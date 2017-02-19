6769cda9ba2c2d0948a822fd931ec6710efb053d
nish97 <sanjiv.nishanth@gmail.com>
2017-02-20 00:48:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,11 @@ button.onclick = function() {
         {
             if(request.Status==200){
                 var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
             }
         }
     };
+    request.open('GET','http://nish97.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
