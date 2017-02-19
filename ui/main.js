7ac17ebc02228297c58b07ae33abc250fa714070
nish97 <sanjiv.nishanth@gmail.com>
2017-02-20 01:18:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,15 +2,17 @@ var button = document.getElementById('counter');
 button.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE)
+        if(request.readyState==XMLHttpRequest.DONE)
         {
-            if(request.Status === 200){
-                var counter =request.responseText;
-                var span =document.getElementById('count');
+            if(request.status==200)
+            {
+                var counter=request.responseText;
+                var span = document.getElementById('count');
                 span.innerHTML=counter.toString();
-            }
+                      
+          }
         }
     };
-    request.open('GET','http://nish97.imad.hasura-app.io/counter',true);
-    request.send(null);
+     request.open('GET','http://nish97.imad.hasura-app.io/counter',true)
+     request.send(null);
 };
