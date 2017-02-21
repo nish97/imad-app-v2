442ae1ad3e2321fcf148c6fb1dc11a34887eb72b
nish97 <sanjiv.nishanth@gmail.com>
2017-02-21 22:48:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,12 @@ function createTemplate(data){
   return htmlTemplate;
 
 }
+var names= [];
+app.get('/submit-name',function (req,res){
+  var name = req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
 var counter= 0;
 app.get('/counter',function(req,res) {
     counter=counter + 1;
@@ -83,12 +89,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var names= [];
-app.get('/submit-name',function (req,res){
-  var name = req.query.name;
-  names.push(name);
-  res.send(JSON.stringify(names));
-});
+
 app.get('/:ArticleName', function (req, res) {
   var ArticleName=req.params.ArticleName;
   res.send(createTemplate(articles[ArticleName]));
