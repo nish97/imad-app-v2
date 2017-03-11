ec6d121af30e9be1eb4efa6163ead9f5301abebd
nish97 <sanjiv.nishanth@gmail.com>
2017-03-11 22:59:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articles/:ArticleName', function (req, res) {
-  pool.query("SELECT * FROM article WHERE Title ='"+ req.params.ArticleName+"'",function(err, result){
+  pool.query("SELECT * FROM article WHERE Title = '"+ req.params.ArticleName+"'",function(err, result){
       if(err){
       res.status(500).send(err.toString());
       } else{
