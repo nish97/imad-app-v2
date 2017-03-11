e73773f5e676f553b283b28db24415699e7c632a
nish97 <sanjiv.nishanth@gmail.com>
2017-03-11 23:04:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,11 +119,12 @@ app.get('/articles/:ArticleName', function (req, res) {
       } else{
       if(result.rows.length ===0){
           res.status(404).send(err.toString());
-      } else{
+      }
+      else{
       var ArticleData = result.rows[0];
       res.send(createTemplate(ArticleData));
       }
-    }
+     }
   });
 });
 app.get('/ui/style.css', function (req, res) {
